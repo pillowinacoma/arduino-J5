f115c468d09eb77ed5e998a3fd6804123ee5bf77
Bechir <nasr.bechir.1997@gmail.com>
2022-01-18 19:03:29 +0100

    client get weather from server

--- client/components/ContentContainer.tsx
+++ client/components/ContentContainer.tsx
@@ -1,5 +1,14 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ContentContainer = () => {
+  useEffect(() => {
+    axios.get(`/weather`).then((res) => {
+        console.log(res.data)
+        console.log(res.data.temp)
+    })
+}, [])
+
   return (
     <div>
     </div>
