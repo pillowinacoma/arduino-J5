6137669eee44a83b03b54c40e552401a250a687f
Bechir <nasr.bechir.1997@gmail.com>
2022-01-18 21:02:46 +0100

    Modif client

--- client/components/ContentContainer.tsx
+++ client/components/ContentContainer.tsx
@@ -34,7 +34,7 @@ const ContentContainer = () => {
             <section className="bg-white border-b py-8">
                 <div className="container mx-auto flex flex-wrap pt-4 pb-12">
                     <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-                        Title
+                        Chauffage autonome
                     </h1>
                     <div className="w-full mb-4">
                         <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
@@ -49,19 +49,21 @@ const ContentContainer = () => {
                                     xGETTING STARTED
                                 </p>
                                 <div className="w-full font-bold text-xl text-gray-800 px-6">
-                                    Lorem ipsum dolor sit amet.
+                                    Mode économie.
                                 </div>
                                 <p className="text-gray-800 text-base px-6 mb-5">
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Aliquam at ipsum eu nunc
-                                    commodo posuere et sit amet ligula.
+                                    Le mode économie d'énergie, alternera
+                                    l'activité du chauffage et de la
+                                    climatisation toutes les 5 secondes. De plus
+                                    la fenêtre de température utilisé pour la
+                                    régulation sera plus grande.
                                 </p>
                             </a>
                         </div>
                         <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
                             <div className="flex items-center justify-start">
                                 <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                                    Action
+                                    Activer
                                 </button>
                             </div>
                         </div>
@@ -79,16 +81,17 @@ const ContentContainer = () => {
                                     Lorem ipsum dolor sit amet.
                                 </div>
                                 <p className="text-gray-800 text-base px-6 mb-5">
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Aliquam at ipsum eu nunc
-                                    commodo posuere et sit amet ligula.
+                                    Le mode activé, s'occupera d'allumer le
+                                    chauffage ou la climatisation pour réguler
+                                    la température en fonction de la température
+                                    extérieur.
                                 </p>
                             </a>
                         </div>
                         <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
                             <div className="flex items-center justify-center">
                                 <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                                    Action
+                                    Activer
                                 </button>
                             </div>
                         </div>
@@ -106,16 +109,17 @@ const ContentContainer = () => {
                                     Lorem ipsum dolor sit amet.
                                 </div>
                                 <p className="text-gray-800 text-base px-6 mb-5">
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Aliquam at ipsum eu nunc
-                                    commodo posuere et sit amet ligula.
+                                    Le mode vacances quant à lui, désactivera le
+                                    chauffage et la climatisation. Ce mode
+                                    pourra être activé ou désactivé en
+                                    actionnant un bouton.
                                 </p>
                             </a>
                         </div>
                         <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
                             <div className="flex items-center justify-end">
                                 <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                                    Action
+                                    Activer
                                 </button>
                             </div>
                         </div>
