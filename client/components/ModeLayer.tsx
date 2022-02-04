061f03802d2889ba2132a12b98436d9e3b0ab95b
Bechir <nasr.bechir.1997@gmail.com>
2022-02-04 12:09:47 +0100

    Modif button style

--- client/components/ModeLayer.tsx
+++ client/components/ModeLayer.tsx
@@ -5,7 +5,6 @@ import { setMode } from '../slices/app'
 import { AppDispatch } from '../store'
 import { useAppSelector } from '../hooks'
 
-
 interface WeatherProps {
     name: Mode
     description: string
@@ -20,30 +19,35 @@ const ModeCard: FC<WeatherProps> = ({
     button_position,
     is_current_mode = false,
 }) => {
-
-    const current_mode = useAppSelector(
-        (state) => state.mode
-    )
+    const current_mode = useAppSelector((state) => state.mode)
 
     const dispatch = useDispatch<AppDispatch>()
     return (
         <div className="p-6">
             <div className="m-6 indicator">
-                <div className={`indicator-item badge badge-secondary ${(current_mode === name) ? '' : 'indicator-item-disable'}`}></div>
+                <div
+                    className={`indicator-item badge badge-secondary ${
+                        current_mode === name ? '' : 'indicator-item-disable'
+                    }`}
+                ></div>
                 <div className="w-full md:w-1/3 flex flex-col flex-grow flex-shrink">
                     <div className="collapse w-96 border rounded-box border-base-300 collapse-arrow">
-                        <input type="checkbox"/>
+                        <input type="checkbox" />
                         <div className="collapse-title w-full font-bold text-xl text-gray-800 px-6">
-                            {name}. <br/>
-                        </div> 
-                        <div className="collapse-content text-gray-800"> 
+                            {name}. <br />
+                        </div>
+                        <div className="collapse-content text-gray-800">
                             {description}
                         </div>
                     </div>
                     <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
                         <div className={`flex items-center ${button_position}`}>
                             <button
-                                className={`mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out ${(name === current_mode) ? "disable-button": ""}`}
+                                className={`mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out ${
+                                    name === current_mode
+                                        ? 'opacity-50 cursor-not-allowed'
+                                        : ''
+                                }`}
                                 onClick={() => {
                                     dispatch(setMode(name))
                                 }}
